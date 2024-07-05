a2a3fac3e01f49b5973de95e1949fab3397457c1
mootattar <mootattar@gmail.com>
2024-07-06 02:32:39 +0300

    first commit

---
+++ src/api/pruduct/controllers/pruduct.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pruduct controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::pruduct.pruduct');
---
+++ src/api/pruduct/services/pruduct.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pruduct service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::pruduct.pruduct');
